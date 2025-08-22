e7ac221f4e122778405bb91f070739474dfcf5e2
Avery Raulston <averyraulston01@protonmail.com>
2025-08-22 15:13:21 -0500

    finished neovide config (I think)

--- snowDay/.config/nvim/lua/neovide.lua
+++ snowDay/.config/nvim/lua/neovide.lua
@@ -2,14 +2,16 @@ vim.g.neovide_opacity = 0.8
 vim.g.neovide_window_blurred = true
 vim.g.neovide_scale_factor = 1.0
 vim.g.neovide_wayland_scale_factor = 1.0
--- vim.g.neovide_floating_blur_amount_x = 9.0
--- vim.g.neovide_floating_blur_amount_y = 9.0
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
 vim.g.neovide_cursor_vfx_particle_density = 2.0
 vim.g.neovide_cursor_vfx_particle_phase = 10.0
-
-
 vim.o.guifont = "JetBrainsMono Nerd Font:h12"
 
+vim.api.nvim_create_autocmd("InsertEnter", {
+callback = function() vim.g.neovide_cursor_vfx_mode = "" end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+callback = function() vim.g.neovide_particle_effect = "railgun" end,
+})
